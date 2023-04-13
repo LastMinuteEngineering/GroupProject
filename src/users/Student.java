b7c6da8ba699b2b72bfd6bb9406e2e6f12dbd529
systeminspired <55170547+systeminspired@users.noreply.github.com>
2023-04-13 09:33:11 -0400

    a

--- src/users/Student.java
+++ src/users/Student.java
@@ -4,6 +4,13 @@ public class Student extends UserAccount {
 
 	protected float gpa;
 	
+	// Operations shown below
+	
+	public void addAdivser(UserAccount adviser) {
+		// grab the adviser and add them to a list
+		// that this class stores
+	}
+	
 	@Override
 	public String getAccountDetails(Boolean fullDetails) {
 		// TODO Auto-generated method stub
