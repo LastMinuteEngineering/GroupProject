1725fbc9d8adcb8bdaa3d1c05a2a796dcca3df82
systeminspired <55170547+systeminspired@users.noreply.github.com>
2023-04-13 09:29:20 -0400

    started making changes at student

--- src/users/Student.java
+++ src/users/Student.java
@@ -2,6 +2,8 @@ package users;
 
 public class Student extends UserAccount {
 
+	protected float gpa;
+	
 	@Override
 	public String getAccountDetails(Boolean fullDetails) {
 		// TODO Auto-generated method stub
