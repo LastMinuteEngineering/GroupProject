073b4a66f2da23377c1e97f0727acbd394b68f73
systeminspired <55170547+systeminspired@users.noreply.github.com>
2023-04-13 12:16:23 -0400

    added complex content

---
+++ src/common/types/ComplexContent.java
@@ -0,0 +1,12 @@
+package common.types;
+
+public class ComplexContent {
+	private String label;
+	private String input;
+	private String filename;
+	
+	public ComplexContent(String label, String input) {
+		this.label = label;
+		this.input = input;
+	}
+}
--- src/users/AccountGroup.java
+++ src/users/AccountGroup.java
@@ -1,6 +1,11 @@
 package users;
 
 public class AccountGroup {
-
-	// TODO
+	private String name;
+	private String id;
+	private String Description;
+	
+	public UserAccount[] getMembers();
+	
+	public UserAccount addMember;
 }
