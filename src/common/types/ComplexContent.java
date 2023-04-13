829ca76627e594749e4909940abdb1115ed67e81
systeminspired <55170547+systeminspired@users.noreply.github.com>
2023-04-13 12:45:24 -0400

    edit

--- src/common/types/ComplexContent.java
+++ src/common/types/ComplexContent.java
@@ -1,12 +1,9 @@
 package common.types;
 
 public class ComplexContent {
-	private String label;
-	private String input;
 	private String filename;
 	
-	public ComplexContent(String label, String input) {
-		this.label = label;
-		this.input = input;
+	public ComplexContent(String filename) {
+		this.filename = filename;
 	}
 }
