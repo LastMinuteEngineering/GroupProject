defa3c59f9100357fe5a59727c281c492f80d494
systeminspired <55170547+systeminspired@users.noreply.github.com>
2023-04-14 15:25:38 -0400

    complex content updated

--- src/common/types/ComplexContent.java
+++ src/common/types/ComplexContent.java
@@ -1,9 +1,17 @@
 package common.types;
 
 public class ComplexContent {
-	private String filename;
+	public String filename;
+	public String format;
+	public String headerData;
+	public String payloadData;
+	public String trailerData;
 	
-	public ComplexContent(String filename) {
+	public ComplexContent(String filename, String format, String headerData, String payloadData, String trailerData) {
 		this.filename = filename;
+		this.format = format;
+		this.headerData = headerData;
+		this.payloadData = payloadData;
+		this.trailerData = trailerData;
 	}
 }
